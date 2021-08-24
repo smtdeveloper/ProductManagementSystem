3bd5ee3bc2f6481a800b98fa63e6d55d22756b49
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-08-24 20:54:53 +0300

    Category acıklama özelliği  eklendi

--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -11,10 +11,8 @@ namespace Entities.Concrete
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         
-        // public string Desription { get; set; }
-       
-
-        //  public byte[] Picture { get; set; }
+        public string Description { get; set; }
+        //public byte[] Picture { get; set; }
 
     }
 }
