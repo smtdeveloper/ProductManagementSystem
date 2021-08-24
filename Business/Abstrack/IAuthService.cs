5ac41daafcb474cf0e203c5e6e4e8d3b7270b1bc
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-08-25 02:03:09 +0300

    user için control yazıldı ve Auth commitlendi

--- Business/Abstrack/IAuthService.cs
+++ Business/Abstrack/IAuthService.cs
@@ -1,18 +1,18 @@
-﻿using Core.Entities.Concrete;
-using Core.Utilities.Results;
-using Core.Utilities.Security.JWT;
-using Entities.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using Core.Entities.Concrete;
+//using Core.Utilities.Results;
+//using Core.Utilities.Security.JWT;
+//using Entities.DTOs;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Business.Abstrack
-{
-    public interface IAuthService
-    {
-        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
-        IDataResult<User> Login(UserForLoginDto userForLoginDto);
-        IResult UserExists(string email);
-        IDataResult<AccessToken> CreateAccessToken(User user);
-    }
-}
+//namespace Business.Abstrack
+//{
+//    public interface IAuthService
+//    {
+//        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
+//        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+//        IResult UserExists(string email);
+//        IDataResult<AccessToken> CreateAccessToken(User user);
+//    }
+//}
--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -1,61 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Business.Abstrack;
-using Entities.DTOs;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Business.Abstrack;
+//using Entities.DTOs;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace WebAPI.Controllers
-{
+//namespace WebAPI.Controllers
+//{
 
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : Controller
-    {
-        private IAuthService _authService;
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class AuthController : Controller
+//    {
+//        private IAuthService _authService;
 
-        public AuthController(IAuthService authService)
-        {
-            _authService = authService;
-        }
+//        public AuthController(IAuthService authService)
+//        {
+//            _authService = authService;
+//        }
 
-        [HttpPost("login")]
-        public ActionResult Login(UserForLoginDto userForLoginDto)
-        {
-            var userToLogin = _authService.Login(userForLoginDto);  
-            if (!userToLogin.Success)
-            {
-                return BadRequest(userToLogin.Message);
-            }
+//        [HttpPost("login")]
+//        public ActionResult Login(UserForLoginDto userForLoginDto)
+//        {
+//            var userToLogin = _authService.Login(userForLoginDto);
+//            if (!userToLogin.Success)
+//            {
+//                return BadRequest(userToLogin.Message);
+//            }
 
-            var result = _authService.CreateAccessToken(userToLogin.Data);
-            if (result.Success)
-            {
-                return Ok(result.Data);
-            }
+//            var result = _authService.CreateAccessToken(userToLogin.Data);
+//            if (result.Success)
+//            {
+//                return Ok(result.Data);
+//            }
 
-            return BadRequest(result.Message);
-        }
+//            return BadRequest(result.Message);
+//        }
 
-        [HttpPost("register")]
-        public ActionResult Register(UserForRegisterDto userForRegisterDto)
-        {
-            var userExists = _authService.UserExists(userForRegisterDto.Email);
-            if (!userExists.Success)
-            {
-                return BadRequest(userExists.Message);
-            }
+//        [HttpPost("register")]
+//        public ActionResult Register(UserForRegisterDto userForRegisterDto)
+//        {
+//            var userExists = _authService.UserExists(userForRegisterDto.Email);
+//            if (!userExists.Success)
+//            {
+//                return BadRequest(userExists.Message);
+//            }
 
-            var registerResult = _authService.Register(userForRegisterDto, userForRegisterDto.Password);
-            var result = _authService.CreateAccessToken(registerResult.Data);
-            if (result.Success)
-            {
-                return Ok(result.Data);
-            }
+//            var registerResult = _authService.Register(userForRegisterDto, userForRegisterDto.Password);
+//            var result = _authService.CreateAccessToken(registerResult.Data);
+//            if (result.Success)
+//            {
+//                return Ok(result.Data);
+//            }
 
-            return BadRequest(result.Message);
-        }
-    }
-}
+//            return BadRequest(result.Message);
+//        }
+//    }
+//}
